60a1e1d5c576a49768fa4665753cb236649ea736
Miguel <>
2019-10-31 08:14:10 -0500

    Cree el proyecto en netbeans de la agenda

---
+++ AgendaPruebaM/src/agendapruebam/AgendaPruebaM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agendapruebam;
+
+/**
+ *
+ * @author Fundamentos DS
+ */
+public class AgendaPruebaM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
